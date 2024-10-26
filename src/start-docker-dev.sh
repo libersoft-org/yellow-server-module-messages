d743f432ae55c98c34ef6fc79b995ea190a460b8
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-10-27 01:37:13 +0200

    docker..

--- src/start-docker-dev.sh
+++ src/start-docker-dev.sh
@@ -2,8 +2,7 @@
 
 [ ! -d "./node_modules/" ] && bun i
 
-rm -rf ./node_modules/yellow-server-common
-ln -s ../../../yellow-server-common ./node_modules/
+rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
 
 bun module-messages.js --create-database
 bun --watch module-messages.js
