aac3c7f34fe8dc1fbddd453149b4de12039c2094
Liberland Software Foundation <info@libersoft.org>
2024-12-13 19:40:46 +0100

    mmm

--- src/start-hot-screen.sh
+++ src/start-hot-screen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ ! -d "./node_modules/" ] && bun i
-screen -dmS yellow-module-messages bash -c '
+screen -dmS yellow-server-module-messages bash -c '
 echo -ne "\033]0;YELLOW MODULE MESSAGES\007"
 bun --watch module-messages.js
 '
