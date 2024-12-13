3e88c2e3d6fbb92fa4adbfeeff7d79e4556a491f
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-14 00:27:09 +0100

    colors

--- src/start-hot-screen.sh
+++ src/start-hot-screen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-screen -dmS yellow-server-module-messages bash -c "trap bash SIGINT; (./start-hot.sh ; bash);"
+screen -dmS yellow-server-module-messages bash -c ". ./colors.sh; trap bash SIGINT; (./start-hot.sh ; bash);"
