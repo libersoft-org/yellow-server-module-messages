d39bfb83dc44b99cf83fe22c1211aa955465627d
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-13 23:51:36 +0100

    start-hot

--- src/start-hot-screen.sh
+++ src/start-hot-screen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-bun i
-screen -dmS yellow-module-messages bash -c '
-echo -ne "\033]0;YELLOW MODULE MESSAGES\007"
-bun --watch module-messages.js
-'
+screen -dmS yellow-module-messages bash -c "trap bash SIGINT; (./start-hot.sh ; bash);"
