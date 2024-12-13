b265c1b1d35866aaa5a583db8223713e52551c64
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-13 14:44:15 +0100

    always bun i

--- src/start-hot-screen.sh
+++ src/start-hot-screen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ ! -d "./node_modules/" ] && bun i
+bun i
 screen -dmS yellow-module-messages bash -c '
 echo -ne "\033]0;YELLOW MODULE MESSAGES\007"
 bun --watch module-messages.js
--- src/start-hot.sh
+++ src/start-hot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[ ! -d "./node_modules/" ] && bun i
+bun i
 echo -ne "\033]0;YELLOW MODULE MESSAGES\007"
 bun --watch module-messages.js
