4f87e3891806f21e4cc37fc8f41bf3d1914b22b8
Your Name <koom@c1>
2025-02-10 16:59:00 +0100

    always install

--- src/start-docker-dev.sh
+++ src/start-docker-dev.sh
@@ -2,7 +2,8 @@
 
 mkdir -p uploads/message-attachments/
 
-[ ! -d "./node_modules/" ] && bun i --frozen-lockfile
+#[ ! -d "./node_modules/" ] &&
+bun i --frozen-lockfile
 rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
 bun module-messages.js --create-database
 bun --watch module-messages.js
