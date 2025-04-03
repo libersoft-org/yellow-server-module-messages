beaf1e345258f319e3df039274f7f4711c2cd3fb
Your Name <koom@c1>
2025-04-03 14:44:57 +0200

    docker fix..

--- start-docker-dev.sh
+++ start-docker-dev.sh
@@ -6,10 +6,10 @@ mkdir -p uploads/message-attachments/
 
 #[ ! -d "./node_modules/" ] &&
 ~/.bun/bin/bun i --frozen-lockfile
-rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
+rm -rf ../node_modules/yellow-server-common; ln -s ../../../yellow-server-common ../node_modules/yellow-server-common
 ~/.bun/bin/bun module-messages.js --create-database
 while true; do
-  ~/.bun/bin/bun --watch src/module-messages.js
+  ~/.bun/bin/bun --watch module-messages.js
   echo "exit code: $?"
   sleep 1
   echo "restarting..."
