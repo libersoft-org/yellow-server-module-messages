18c5feaaa1f1057b008e92de3fd5648dae94cf1f
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-04-10 13:17:29 +0200

    replace --create-database with knex:migrate

--- start-docker-dev.sh
+++ start-docker-dev.sh
@@ -7,7 +7,7 @@ mkdir -p uploads/message-attachments/
 #[ ! -d "./node_modules/" ] &&
 ~/.bun/bin/bun i --frozen-lockfile
 rm -rf ../node_modules/yellow-server-common; ln -s ../../../yellow-server-common ../node_modules/yellow-server-common
-~/.bun/bin/bun module-messages.js --create-database
+~/.bun/bin/bun run knex:migrate
 while true; do
   ~/.bun/bin/bun --watch module-messages.js
   echo "exit code: $?"
